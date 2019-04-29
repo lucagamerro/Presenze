0a061b9b4bb9e06518e2feecb9aa00a98d0887b2
Luca Gamerro <43759024+lucagamerro@users.noreply.github.com>
2019-04-29 21:02:30 +0200

    Update informazioni.php

--- informazioni.php
+++ informazioni.php
@@ -1,13 +1,42 @@
 <?php 
+$nome = $_GET['nome'];
+$sq= $_GET['sq'];
+$si = $_GET['si'];
+$nontrovato = " ";
+$verra = "";
+$sp = ' 
+';
+if ($nome == ""):
+    $nontrovto = ".";
+endif;
 if (date('j') == 1):
     unlink("presenze.csv");
     $f = fopen("presenze.csv", "a");
     fwrite($f, "NOME ;");
-    fwrite($f, "PRESENZA ");
+    fwrite($f, "SQUADRIGLIA ; ");
     fwrite($f, $sp);
     fclose($f); 
 endif;
+
+if ($si == 'si'):
+    $verra = "Si";
+else:
+    $verra = "No";
+endif;
+    
+
+$f = fopen("presenze.csv", "a");
+fwrite($f, $nome);
+fwrite($f, ';');
+fwrite($f, $sq);
+fwrite($f, ';');
+fwrite($f, $verra);
+fwrite($f, ';');
+fwrite($f, $sp);
+fclose($f);
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,33 +54,31 @@ endif;
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="index.html"> <i class="fas fa-home"></i> Home </a>
+        <a class="nav-link" href="index.html"> <i class="fas fa-home"></i> Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="vedi.php"><i class="fas fa-table"></i> Presenze <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="vedi.php"><i class="fas fa-table"></i> Presenze</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="informazioni.php"><i class="fas fa-info-circle"></i> Informazioni </a>
+        <a class="nav-link" href="informazioni.php"><i class="fas fa-info-circle"></i> Informazioni</a>
       </li>
     </ul>
   </div>
 </nav>
+<br> 
 <br>
-<br>
-<div class="jumbotron">
-  <h1 class="display-3">Informazioni!</h1>
-  <p class="lead"> Sviluppo, idee, contatti e altro</p>
-  <hr class="my-4">
-  <p> Innazitutto, ci tengo molto a evidenziare il fatto che tutto questo sito, progettato mentalmente durante il campetto di Pasqua di maggio, si nativamente e interamente Open Surce. Per i meno esperti vuol dire:</p>
-  <p class="lead">
-  <blockquote class="blockquote text-center">
-  <i>In informatica il termine inglese open source (che significa sorgente aperta) viene utilizzato per riferirsi ad un software di cui i detentori dei diritti rendono pubblico il codice sorgente, favorendone il libero studio e permettendo a programmatori indipendenti di apportarvi modifiche ed estensioni.</i>
-  <footer class="blockquote-footer"> Wikipedia </cite></footer>
-  </blockquote>
-  <p> Questo valore, nonostante puramente informatico, si rispecchia bene nell'idea di comunita' che ho appreso attraverso gli scout. </p> <br>
-    <a class="btn btn-primary btn-lg" href="https://github.com/lucagamerro" role="button"> <i class="fab fa-github"></i>  Codice su GitHub </a> <br> <br>
-  <p>  Infine, se volete contattarmi, potete farlo da <a type="button" class="btn btn-primary btn-sm" href="https://lucagamerro.carrd.co"> QUI </a></p>
-  </p>
+<?php if ($nontrovato == " "): ?>
+<div class="alert alert-dismissible alert-warning">
+     <h4 class="alert-heading"> Inviato! </h4>
+     <p class="mb-0"> Presenza inviata correttamente. Ricorda di utilizzare questo trumento in modo sicuro e responsabile. </p>
 </div>
-<small class="form-text text-muted"> Creato da Luca Gamerro. Codice rilasciato sotto MIT License.</small>
+<?php endif; ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<small class="form-text text-muted"> Creato da Luca Gamerro. Codice rilasciato sotto MIT License. </small>
 </html>
